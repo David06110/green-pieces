5f17c5ca4921912e8811734a21a20b6b97a7f758
AudreyBerger <berger.audrey06@gmail.com>
2021-08-18 11:18:31 +0200

    Started landing

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   #get 'spaces/update'
   #get 'spaces/destroy'
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get '/landing', to:"pages#landing"
 end
