5ab17453e6c85db7ab39b9273b59cf144b8090f1
davidhaffner <david.haffner06@gmail.com>
2021-08-18 15:49:04 +0200

    debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ p test1
 p test2
 # Seed pour Spaces
 p "Creation du coin de paradis"
-space0 = Space.create(type_of: "green", name: "Golf ready", check_in: "16/08/21 15h", check_out: "17/08/21 15h", price: "150€",user_id: test0.id)
+space0 = Space.create(type_of: "greens", name: "Golf ready", check_in: "16/08/21 15h", check_out: "17/08/21 15h", price: "150€",user_id: test0.id)
 p space0
 space1 = Space.create(type_of: "pink", name: "big pool", check_in: "16/08/21 15h", check_out: "17/08/21 15h", price: "1500€",user_id: test1.id)
 p space1
 space2 = Space.create(type_of: "blue", name: "corner for tomatoes", check_in: "16/08/21 15h", check_out: "17/08/21 15h", price: "220€",user_id: test2.id)
 p space2
 
-p "booking test"
-booking0 = Booking.create!(user: test0, space: space0)
-p booking0
+# p "booking test"
+# booking0 = Booking.create!(user: test0, space: space0)
+# p booking0
