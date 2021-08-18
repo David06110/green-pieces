61f818890cd0d0a83563922a2755d795899398cc
davidhaffner <david.haffner06@gmail.com>
2021-08-18 12:29:09 +0200

    ici taout va bien

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,11 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
-    @booking.user_id = current_user.id
-    @booking.space_id = 6 #params[space.id]
-    @booking_new = @booking.save!
-    #Booking.create(user_id: current_user, space_id: 6)
-    redirect_to booking_path(Booking.last.id)
+    # @booking = Booking.new
+    # @booking.user_id = current_user.id
+    # @booking.space_id = 6 #params[space.id]
+    # @booking_new = @booking.save!
+    @booked = Booking.create(user_id: current_user, space_id: params[space.id])
+    redirect_to booking_path(@booked.id)
   end
 
   def create
